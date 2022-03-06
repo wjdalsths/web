929ea12b59043a2d4081f04c2acba5f421580ba8
Jeongmin <86366459+wjdalsths@users.noreply.github.com>
2022-03-06 17:16:58 +0900

    bar scroll test

---
+++ practice/BarTest/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // let wrap = document.querySelector(".wrap");
+    // let bar = document.querySelector('.bar');
+    // let per = document.querySelector('.per');
+    
+    // wrap.addEventListener('scroll', function(){
+    //     let scrollTop = wrap.scrollTop;
+    //     let scrollHeight = wrap.scrollHeight - wrap.clientHeight;
+    //     let percentage = (scrollTop/scrollHeight) * 100;
+    //     if(isNaN(percentage)) percentage = 0;
+    //     bar.style.width = percentage + '%';
+    //     per.innerText = Math.floor(percentage) + '%';
+    // })
+})
